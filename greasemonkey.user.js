25ec45bdb2e5ad228da374e027cb3e6d31c2a0b6
Andrea <andry25.9.99@gmail.com>
2018-06-30 15:18:54 +0100

    First fix

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -4,15 +4,18 @@
 // @include     https://twitter.com/*
 // @include     https://tweetdeck.com/*
 // @include     https://tweetdeck.twitter.com/*
-// @version     1
+// @include     https://t.co/*
+// @version     0.1
 // @grant       none
 // ==/UserScript==
+
+var serveraddr = "http://207.154.195.181:8081"
 
 var observer = new MutationObserver(function(mutations) {
   var aTags = document.body.getElementsByTagName("a");
   for (var i = 0; i < aTags.length; i++) {
     var tag = aTags[i];
-    
+    console.log(tag);
     if (tag.getAttribute("class") !== null && tag.getAttribute("class").indexOf("twitter-atreply") > -1) {
       continue;
     }
@@ -24,6 +27,9 @@ var observer = new MutationObserver(function(mutations) {
         tag.href = tag.getAttribute("data-full-url");
       } else if (tag.getAttribute("title") !== "" && tag.getAttribute("title") !== null) {
         tag.href = tag.getAttribute("title");
+      } else {
+        //http://207.154.195.181 is A TEMPORARY SERVER - ITS WORKING IS NOT GUARANTEED 
+        tag.href = tag.href.replace("https://t.co", serveraddr);
       }
     }
   };
